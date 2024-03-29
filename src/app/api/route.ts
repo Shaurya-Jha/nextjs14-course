bbc6baa5bcdc56c95bb474a466a914bb22de9a3f
Shaurya Jha <76627647+Shaurya-Jha@users.noreply.github.com>
2024-03-29 10:38:36 +0530

    working with API routes

---
+++ src/app/api/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+// export async function GET () {
+//     return NextResponse.json({
+//         message:"Hello world"
+//     })
+// }
+
+export async function POST () {
+    return NextResponse.json({
+        message:"POST method"
+    })
+}
